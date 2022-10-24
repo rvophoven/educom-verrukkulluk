f0c7c2d785e472eb4221536c74e682d9fce9485b
Ramon van Opoven <rvophoven@gmail.com>
2022-10-24 11:14:24 +0200

    add composer/twig (#13)

--- index.php
+++ index.php
@@ -13,16 +13,20 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 
 /******************************/
 
+
+
 /// Next step, iets met je data doen. Ophalen of zo
 require_once("lib/gerecht.php");
 $gerecht = new gerecht();
 $data = $gerecht->selecteerGerecht();
 
 
+
 /*
 URL:
 http://localhost/index.php?gerecht_id=4&action=detail
 */
+
 
 $gerecht_id = isset($_GET["gerecht_id"]) ? $_GET["gerecht_id"] : "";
 $action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
@@ -56,3 +60,5 @@ $template = $twig->load($template);
 
 /// En tonen die handel!
 echo $template->render(["title" => $title, "data" => $data]);
+
+
